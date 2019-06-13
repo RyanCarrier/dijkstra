fef8ebdfb93a5e4a25e328463f2bd0d2974538b1
Ryan Carrier <ryan.j.carrier@gmail.com>
2019-06-13 22:41:38 +1000

    actually add file...

---
+++ dijkstra_all.go
@@ -0,0 +1,70 @@
+package dijkstra
+
+//Shortest calculates the shortest path from src to dest
+func (g *Graph) ShortestAll(src, dest int) (BestPaths, error) {
+	return g.evaluateAll(src, dest, true)
+}
+
+//Longest calculates the longest path from src to dest
+func (g *Graph) LongestAll(src, dest int) (BestPaths, error) {
+	return g.evaluateAll(src, dest, false)
+}
+
+func (g *Graph) evaluateAll(src, dest int, shortest bool) (BestPaths, error) {
+	//Setup graph
+	g.setup(shortest, src, -1)
+	return g.postSetupEvaluateAll(src, dest, shortest)
+}
+
+func (g *Graph) postSetupEvaluateAll(src, dest int, shortest bool) (BestPaths, error) {
+	var current *Vertex
+	oldCurrent := -1
+	solutions := BestPaths{}
+	for g.visiting.Len() > 0 {
+		//Visit the current lowest distanced Vertex
+		current = g.visiting.PopOrdered()
+		if oldCurrent == current.ID {
+			continue
+		}
+		oldCurrent = current.ID
+		//If the current distance is already worse than the best try another Vertex
+		if shortest && current.distance >= g.best { //} || (!shortest && current.distance <= g.best) {
+			continue
+		}
+		for v, dist := range current.arcs {
+			//If the arc has better access, than the current best, update the Vertex being touched
+			if (shortest && current.distance+dist < g.Verticies[v].distance) ||
+				(!shortest && current.distance+dist > g.Verticies[v].distance) {
+				//if g.Verticies[v].bestVertex == current.ID && g.Verticies[v].ID != dest {
+				if current.bestVertex == v && g.Verticies[v].ID != dest {
+					//also only do this if we aren't checkout out the best distance again
+					//This seems familiar 8^)
+					return BestPaths{}, newErrLoop(current.ID, v)
+				}
+				g.Verticies[v].distance = current.distance + dist
+				g.Verticies[v].bestVertex = current.ID
+				if v == dest {
+					g.visitedDest = true
+					if (shortest && current.distance+dist <= g.best) || (!shortest && current.distance+dist >= g.best) {
+						if current.distance+dist != g.best {
+							g.best = current.distance + dist
+							solutions = BestPaths{}
+						}
+						solutions = append(solutions, g.bestPath(src, dest))
+					}
+					continue
+					//If this is the destination update best, so we can stop looking at
+					// useless Verticies
+				}
+				//Push this updated Vertex into the list to be evaluated, pushes in
+				// sorted form
+				g.visiting.PushOrdered(&g.Verticies[v])
+			}
+		}
+	}
+	if !g.visitedDest {
+		return BestPaths{}, ErrNoPath
+	}
+	//fmt.Println(solutions)
+	return solutions, nil
+}
